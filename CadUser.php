9be25275c957666c29b36497c5814f39b162dd31
Victor Serafim <92189270+VictorHugoSerafim@users.noreply.github.com>
2022-01-12 23:54:48 -0300

    Update and rename valida.php to CadUser.php

---
+++ CadUser.php
@@ -0,0 +1,16 @@
+<?php
+include_once 'conexao.php';
+
+if(isset($_POST['btnCadUsuario'])){
+	$nome = addslashes($_POST['nome']);
+	$email = addslashes($_POST['email']);
+	$senha = addslashes($_POST['senha']);
+	$telefone = addslashes($_POST['telefone']);
+
+	$sql = $pdo->prepare("INSERT INTO 'usuarios' VALUES(null,?,?,?)");
+	$sql->execute(array($nome,$email,$senha,$telefone));
+	header("Location: pagamento.php");
+
+}
+
+?>
--- valida.php
+++
@@ -1,8 +0,0 @@
-<?php
-$mysqli_connection = new MySQLi('uyu7j8yohcwo35j3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'sk219jft4qfiwql9', 'sk219jft4qfiwql9', 'ppmnzoters1z67jj');
-if($mysqli_connection->connect_error){
-   echo "Desconectado! Erro: " . $mysqli_connection->connect_error;
-}else{
-   echo "Conectado!";
-}
-?>
