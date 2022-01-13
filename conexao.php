1ae1095fcd00d6b96b4a56c2235fce9fad5b91d2
Victor Serafim <92189270+VictorHugoSerafim@users.noreply.github.com>
2022-01-12 23:57:02 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,25 +1,15 @@
 <?php
-$servername = "dcrhg4kh56j13bnu.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-$database = "	qdtcpjz1or7zy5km";
-$username = "	kc6pkui3la1tftux";
-$password = "	ocjnu2n93wmjnajz";
-
-// Conexão 
-$conn = mysqli_connect($servername, $username, $password, $database);
-
-// Checar a conexão
-// if (!$conn) {
-//       die("Conexão Falhou: " . mysqli_connect_error());
-// }
- 
-// echo "Conexão completa";
- 
-// $sql = "INSERT INTO Students (name, lastname, email) VALUES ('Test', 'Testing', 'Testing@tesing.com')";
-// if (mysqli_query($conn, $sql)) {
-//       echo "New record created successfully";
-// } else {
-//       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-// }
-// mysqli_close($conn);
+    define('HOST','i0rgccmrx3at3wv3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+    define('DB','aqpr222w7u270hu7');
+    define('USER','tju84nj1hcz7xwhn');
+    define('PASS','escnt6m40aoymihq');
+    
+    try{
+        $pdo = new PDO("mysql:host=".HOST.";dbname=".DB,USER,PASS,array(PDO
+        ::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+        // echo "Conectado";
+    }catch(Exception $e){
+        echo "Nao conectou". $e->getMessage();
+        exit;
+    }
 ?>
-
