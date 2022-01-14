a07b97796d18c5505074706ac2aa8a328119a2ec
Victor Serafim <92189270+VictorHugoSerafim@users.noreply.github.com>
2022-01-13 21:20:17 -0300

    Update CadPag.php

--- CadPag.php
+++ CadPag.php
@@ -1,18 +1,17 @@
 <?php
+include_once 'conexao.php';
 
 if(isset($_POST['btnCadDados'])){
-	include_once 'conexao.php';
-
 	$paymentMethod = addslashes($_POST['paymentMethod']);
 	$nomeCartao = addslashes($_POST['nomeCartao']);
 	$numeroCartao = addslashes($_POST['numeroCartao']);
 	$vencimento = addslashes($_POST['vencimento']);
-    $cvc = addslashes($_POST['cvc']);
-
-	$sql = $pdo->prepare("INSERT INTO 'pagamento' VALUES(null,?,?,?,?,?)");
+	$cvc = addslashes($_POST['cvc']);
+  
+	$sql = $pdo->prepare("INSERT INTO pagamento VALUES(null,?,?,?,?,?)");
 	$sql->execute(array($paymentMethod,$nomeCartao,$numeroCartao,$vencimento,$cvc));
 	header("Location: itens.php");
-
-}
+  
+  }
 
 ?>
