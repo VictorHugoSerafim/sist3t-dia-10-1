422f026f2bb84777da562a8fed0dc733a2af397c
Victor Serafim <92189270+VictorHugoSerafim@users.noreply.github.com>
2022-01-13 21:20:59 -0300

    Update CadUser.php

--- CadUser.php
+++ CadUser.php
@@ -1,18 +1,16 @@
 <?php
+include_once 'conexao.php';
 
 if(isset($_POST['btnCadUsuario'])){
-		
-include_once 'conexao.php';			
-	
 	$nome = addslashes($_POST['nome']);
 	$email = addslashes($_POST['email']);
 	$senha = addslashes($_POST['senha']);
 	$telefone = addslashes($_POST['telefone']);
-
-	$sql = $pdo->prepare("INSERT INTO 'usuarios' VALUES(null,?,?,?)");
+ 
+	$sql = $pdo->prepare("INSERT INTO usuarios VALUES(null,?,?,?,?)");
 	$sql->execute(array($nome,$email,$senha,$telefone));
 	header("Location: pagamento.php");
-
-}
+ 
+ }
 
 ?>
